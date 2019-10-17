f2b1f473246b9e26b5e800fe296713d996790502
Alex Wallace <alexgwallace@gmail.com>
2019-10-17 12:35:42 -0400

    added all input files

--- src/main/java/com/algorand/demo/RunLifecycle.java
+++ src/main/java/com/algorand/demo/RunLifecycle.java
@@ -4,16 +4,41 @@ import com.algorand.utils.MongoStore;
 
 public class RunLifecycle
 {
+    static String[] executionFiles = new String[]{
+            "./Files/UC1_Block_Trade_BT1.json",
+//            "./Files/UC1_Block_Trade_BT2.json",
+//            "./Files/UC1_Block_Trade_BT3.json",
+//            "./Files/UC1_Block_Trade_BT4.json",
+            "./Files/UC1_Block_Trade_BT5.json",
+//            "./Files/UC1_Block_Trade_BT6.json",
+//            "./Files/UC1_Block_Trade_BT7.json",
+            "./Files/UC1_Block_Trade_BT8.json",
+//            "./Files/UC1_Block_Trade_BT9.json",
+//            "./Files/UC1_Block_Trade_BT10.json",
+    };
+    static String[] allocationFiles = new String[]{
+            "./Files/UC2_Allocation_Trade_AT1.json",
+//            "./Files/UC2_Allocation_Trade_AT2.json",
+//            "./Files/UC2_Allocation_Trade_AT3.json",
+//            "./Files/UC2_Allocation_Trade_AT4.json",
+            "./Files/UC2_Allocation_Trade_AT5.json",
+//            "./Files/UC2_Allocation_Trade_AT6.json",
+//            "./Files/UC2_Allocation_Trade_AT7.json",
+            "./Files/UC2_Allocation_Trade_AT8.json",
+//            "./Files/UC2_Allocation_Trade_AT9.json",
+//            "./Files/UC2_Allocation_Trade_AT10.json",
+    };
+    static String[] affirmationFiles = allocationFiles;
+    static String[] confirmationFiles = allocationFiles;
+    static String[] settlementFiles = allocationFiles;
+    static String[] collateralFiles = new String[]{};
+    static String[] reportFiles = new String[]{
+            "./Files/UC6_Portfolio_Instructions_20191016.json",
+            "./Files/UC6_Portfolio_Instructions_20191017.json"
+    };
+
     public static void runLifecycle() throws Exception
     {
-        String[] executionFiles = new String[]{"./Files/UC1_Block_Trade_BT1.json", "./Files/UC1_Block_Trade_BT5.json", "./Files/UC1_Block_Trade_BT8.json"};
-        String[] allocationFiles = new String[]{"./Files/UC2_Allocation_Trade_AT1.json", "./Files/UC2_Allocation_Trade_AT5.json", "./Files/UC2_Allocation_Trade_AT8.json"};
-        String[] affirmationFiles = allocationFiles;
-        String[] confirmationFiles = allocationFiles;
-        String[] settlementFiles = allocationFiles;
-        String[] collateralFiles = new String[]{};
-        String[] reportFiles = new String[]{"./Files/UC6_Portfolio_Instructions_20191016.json", "./Files/UC6_Portfolio_Instructions_20191017.json"};
-
         MongoStore.dropDatabase();
         CommitExecution.main(executionFiles);
         CommitAllocation.main(allocationFiles);
